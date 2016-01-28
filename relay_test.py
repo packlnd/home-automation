8c25eb0010f0da76f3102573591784b390ea76cb
Patrik Ackland <patrikackland@gmail.com>
2016-01-28 01:49:55 -0800

    Made relay test more general

--- relay_test.py
+++ relay_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-import time
+import time,sys
 
 def init_pins(pins):
     for i in pins:
@@ -16,7 +16,9 @@ def test_GPIO(pin_no):
 
 def main():
     GPIO.setmode(GPIO.BCM)
-    pins = [17, 27]
+    if len(sys.argv) < 1:
+        print 'Usage: python relay_test pin1 pin2 pin3'
+    pins = [int(s) for s in sys.argv]
     init_pins(pins)
     for _ in range(5):
         for p in pins:
