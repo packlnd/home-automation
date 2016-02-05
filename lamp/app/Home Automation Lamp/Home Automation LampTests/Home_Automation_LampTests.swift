e9feb15a2fc1438c1f2a1d68571698cb91f7eed3
Patrik Ackland <patrikackland@gmail.com>
2016-02-05 01:13:53 -0800

    Git ignore

---
+++ lamp/app/Home Automation Lamp/Home Automation LampTests/Home_Automation_LampTests.swift
@@ -0,0 +1,36 @@
+//
+//  Home_Automation_LampTests.swift
+//  Home Automation LampTests
+//
+//  Created by Patrik Ackland on 2/5/16.
+//  Copyright © 2016 Patrik Ackland. All rights reserved.
+//
+
+import XCTest
+@testable import Home_Automation_Lamp
+
+class Home_Automation_LampTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
