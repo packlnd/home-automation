f3b26fefd7cad3aabc530bdf349d4fa613d2f46d
Patrik Ackland <patrikackland@gmail.com>
2016-03-12 14:14:15 -0800

    Recreated app structure

---
+++ app/home-automation-controller/home-automation-controllerTests/home_automation_controllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  home_automation_controllerTests.swift
+//  home-automation-controllerTests
+//
+//  Created by Patrik Ackland on 3/12/16.
+//  Copyright © 2016 Patrik Ackland. All rights reserved.
+//
+
+import XCTest
+@testable import home_automation_controller
+
+class home_automation_controllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
