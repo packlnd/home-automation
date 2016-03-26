013ab51f2149f8c1fcdefde9d9c33fb45c2b8c1a
Patrik Ackland <patrikackland@gmail.com>
2016-03-26 16:56:16 -0700

    Wrote js

--- lamp/webserver/static/js/script.js
+++ lamp/webserver/static/js/script.js
@@ -1,5 +1,13 @@
 $(function() {
   $("#myonoffswitch").on('click', function() {
     console.log(this.checked);
+    var ep = this.checked ? "on" : "off";
+    $.ajax({
+      type: "GET",
+      url: "/"+ep,
+      success: function(data) {
+        console.log(data);
+      }
+    })
   });
 });
