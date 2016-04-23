bd5f7ff11f80e4ce631c983892acb715c1e239ac
Patrik Ackland <patrikackland@gmail.com>
2016-04-22 18:02:43 -0700

    Center and other stuff

--- lamp/webserver/static/js/script.js
+++ lamp/webserver/static/js/script.js
@@ -1,5 +1,9 @@
 $(function() {
   $('.timepicker').wickedpicker();
+  $("#input-time").on('input', function() {
+    var time = $("#input-time").text();
+    console.log(time);
+  });
   $("#myonoffswitch").on('click', function() {
     console.log(this.checked);
     var ep = this.checked ? "on" : "off";
