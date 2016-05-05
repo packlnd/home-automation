272b25f8915c1de7a53dd484e24259bc28b11699
Patrik Ackland <patrikackland@gmail.com>
2016-05-04 17:17:18 -0700

    Do this separately

--- lamp/webserver/db.py
+++ lamp/webserver/db.py
@@ -1,8 +1,6 @@
 import sqlite3
 
 db = sqlite3.connect('db.db')
-cur = db.cursor()
-cur.execute("CREATE TABLE Lamp(Id INT, On TEXT, Off TEXT, Duration INT)")
 
 def write_to_db(on, off, duration):
     cur = db.cursor()
