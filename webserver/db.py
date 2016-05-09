c1c00827999d661d868e720a13d72ef65f3ad37c
Patrik Ackland <patrikackland@gmail.com>
2016-05-09 01:40:02 -0700

    Preparing db for alarm

--- webserver/db.py
+++ webserver/db.py
@@ -2,9 +2,12 @@ import sqlite3
 
 db = sqlite3.connect('db.db')
 
-def write_to_db(on, off, duration):
+def write_status_to_db(on, off, duration):
     cur = db.cursor()
-    cur.execute("INSERT INTO Cars VALUES(1,'Audi',52642)")
+    cur.execute("INSERT INTO  VALUES(1,'Audi',52642)")
+
+def write_alarm_to_db(alarm):
+    cur = db.cursor()
 
 def read_from_db():
     cur = db.cursor()
