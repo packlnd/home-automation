8a016b5809ec8fbccf0a09e554cdb4f2c076eab7
Patrik Ackland <patrikackland@gmail.com>
2016-05-17 00:11:56 -0700

    Implemented stocks.py

--- webserver/stocks.py
+++ webserver/stocks.py
@@ -1,1 +1,29 @@
-from yahoo_finance import Share
+from yahoo_finance import Share, Currency
+import json
+
+usdsek = Currency("USDSEK")
+
+def get_stocks():
+    data = None
+    with open('stocks.json') as f:
+        data = json.load(f)
+    populate_prices(data)
+    return data
+
+def populate_prices(data):
+    usdsek.refresh()
+    tot = 0.0
+    for i in range(len(data["stocks"])):
+        t = get_today(data["stocks"][i])
+        data["stocks"][i]["today"] = t
+        tot += t
+    data["total"] = tot
+
+def get_today(stock):
+    s = Share(stock["quote"])
+    sw = float(usdsek.get_rate())
+    val = float(stock["amount"])*(float(s.get_price()) - float(s.get_open()))
+    if stock["curr"] == "USD":
+        val *= sw
+    return val
+
