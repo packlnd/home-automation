1c5def8a3885977a5a706cf5a1c0e10509a9f3d8
Patrik Ackland <patrikackland@gmail.com>
2016-05-29 00:10:30 -0700

    Added tests

--- webserver/test.py
+++ webserver/test.py
@@ -2,12 +2,14 @@ import stocks
 import util
 import alarm
 import logger
-
+import re
+pattern_date = re.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}$")
 ###
 s = stocks.get_stocks()
-assert(len(s)==4)
+assert(len(s)==4 or len(s) == 5)
 for d in s:
     assert(type(d) == str and type(s[d]) == float)
+    assert(pattern_date.match(d))
 ###
 
 ###
