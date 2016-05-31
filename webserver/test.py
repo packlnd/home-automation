57964b861900f720a3916a73ff9c45427e848d13
Patrik Ackland <patrikackland@gmail.com>
2016-05-30 20:27:51 -0700

    New tests

--- webserver/test.py
+++ webserver/test.py
@@ -6,12 +6,13 @@ import re
 pattern_date = re.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}$")
 ###
 s = stocks.get_stocks()
-assert(len(s)==4 or len(s) == 5)
 for d in s:
     assert(type(d) == str and type(s[d]) == float)
     assert(pattern_date.match(d))
 ###
-
+stats = (logger.Logger()).get_stats()
+assert(type(stats) == dict)
+assert(len(stats) == 2)
 ###
 
 print '\nOK.'
