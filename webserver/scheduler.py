4936aa7e54c1dae94093d0ded9a56aa266479f71
Patrik Ackland <patrikackland@gmail.com>
2016-06-07 22:40:13 -0700

    Upd sched

--- webserver/scheduler.py
+++ webserver/scheduler.py
@@ -1,9 +1,8 @@
 from geopy.geocoders import Nominatim
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-def some_job():
+def schedule_lamp_on():
         print "Decorated job"
-
         scheduler = BlockingScheduler()
         scheduler.add_job(some_job, 'interval', hours=1)
         scheduler.start()
