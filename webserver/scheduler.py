947c69498588f16d829357d0dac6a24280af07ea
Patrik Ackland <patrikackland@gmail.com>
2016-06-08 00:14:22 -0700

    last final tmr

--- webserver/scheduler.py
+++ webserver/scheduler.py
@@ -2,7 +2,7 @@ from geopy.geocoders import Nominatim
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def schedule_lamp_on():
-        print "Decorated job"
+        #FIXME: Is this the best way to do this?
         scheduler = BlockingScheduler()
         scheduler.add_job(some_job, 'interval', hours=1)
         scheduler.start()
