26588cefc55f8a27c83f62cf06e62d73306484ca
Patrik Ackland <patrikackland@gmail.com>
2016-06-10 16:27:57 -0700

    Know how to do sched now

--- webserver/scheduler.py
+++ webserver/scheduler.py
@@ -1,8 +1,5 @@
 from geopy.geocoders import Nominatim
-from apscheduler.schedulers.blocking import BlockingScheduler
+from crontab import CronTab
 
 def schedule_lamp_on():
-        #FIXME: Is this the best way to do this?
-        scheduler = BlockingScheduler()
-        scheduler.add_job(some_job, 'interval', hours=1)
-        scheduler.start()
+    raise NotImplementedError
